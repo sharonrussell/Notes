87c0de55a5908f550f33ba3b035d371f07f7378f
sharonrussell <sharon.russell@comparethemarket.com>
2015-09-08 15:34:35 +0100

    Added test for creating note and get note

---
+++ Notes/Domain.Tests/NoteTests.cs
@@ -0,0 +1,17 @@
+﻿using Notes;
+using NUnit.Framework;
+
+namespace Domain.Tests
+{
+    [TestFixture]
+    public class NoteTests
+    {
+        [Test]
+        public void When_CreatingNote_Should_SetTitle()
+        {
+            Note note = new Note("some text");
+
+            Assert.That(note.GetText(), Is.EqualTo("some text"));
+        }
+    }
+}
---
+++ Notes/Domain/Note.cs
@@ -0,0 +1,22 @@
+﻿namespace Notes
+{
+    public class Note
+    {
+        private string _text;
+
+        public Note(string text)
+        {
+            SetText(text);
+        }
+
+        public void SetText(string text)
+        {
+            _text = text;
+        }
+
+        public string GetText()
+        {
+            return _text;
+        }
+    }
+}
